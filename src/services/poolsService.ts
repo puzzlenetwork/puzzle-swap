1cbd68f4dbe76cf6db16bb32e34107d047540c9e
Олексiй <alexnagorny.an@gmail.com>
2022-04-14 06:36:14 +0300

    api-base-to-env

--- src/services/poolsService.ts
+++ src/services/poolsService.ts
@@ -15,11 +15,11 @@ interface IPoolSettings {
 
 const poolService = {
   getPuzzlePools: async (): Promise<IPoolSettings[]> => {
-    await axios.get("https://localhost:5000/api/v1/pools");
+    await axios.get(`${process.env.REACT_APP_API_BASE}/api/v1/pools`);
     return [];
   },
   checkDomain: async (domain: string): Promise<boolean> => {
-    await axios("http://localhost:5000/api/v1/pools/check-domain", {
+    await axios(`${process.env.REACT_APP_API_BASE}/api/v1/pools/check-domain`, {
       method: "POST",
       headers: { "Content-type": "application/json" },
       data: { domain },
@@ -27,11 +27,14 @@ const poolService = {
     return true;
   },
   createPool: async (settings: IPoolSettings): Promise<boolean> => {
-    await axios(`http://localhost:5000/api/v1/pools/pool/${settings.domain}`, {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      data: { settings },
-    });
+    await axios(
+      `${process.env.REACT_APP_API_BASE}/api/v1/pools/pool/${settings.domain}`,
+      {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        data: { settings },
+      }
+    );
     return true;
   },
 };
