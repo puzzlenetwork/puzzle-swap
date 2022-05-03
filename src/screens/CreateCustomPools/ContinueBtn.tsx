f703a9992c789a2f806e990555ba4c5783563c8a
lzhabo <lidia.zhabo99@gmail.com>
2022-05-02 23:44:18 -0500

    fix incorrect code in continue btn

--- src/screens/CreateCustomPools/ContinueBtn.tsx
+++ src/screens/CreateCustomPools/ContinueBtn.tsx
@@ -4,8 +4,6 @@ import { useCreateCustomPoolsVM } from "@screens/CreateCustomPools/CreateCustomP
 import { observer } from "mobx-react-lite";
 import { useStores } from "@stores";
 import Loading from "@components/Loading";
-import bucketService from "@src/services/bucketService";
-import { toFile } from "@src/utils/files";
 
 const ContinueBtn: React.FC = () => {
   const vm = useCreateCustomPoolsVM();
@@ -41,20 +39,9 @@ const ContinueBtn: React.FC = () => {
       );
     case 1:
       return (
-        <Button
-          onClick={async () => {
-            if (vm.logo == null) return;
-            console.log("upload");
-            const image = await bucketService.upload(toFile(vm.logo));
-            console.log(image);
-          }}
-          fixed
-        >
-          Upload
+        <Button onClick={() => handleContinue(2)} fixed disabled={!vm.correct1}>
+          {vm.correct1 ? "Continue" : "Fill in all fields"}
         </Button>
-        // <Button onClick={() => handleContinue(2)} fixed disabled={!vm.correct1}>
-        //   {vm.correct1 ? "Continue" : "Fill in all fields"}
-        // </Button>
       );
     case 2:
       return (
