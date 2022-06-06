9608c194fdec037df14a789b2cc47c31d58cb8a5
lzhabo <lidia.zhabo99@gmail.com>
2022-06-06 12:15:31 +0100

    change tPuzzle to Puzzle token in custom pool creation page

--- src/screens/CreateCustomPools/PoolSettingsCard/ConfirmPoolCreation/NoPayment.tsx
+++ src/screens/CreateCustomPools/PoolSettingsCard/ConfirmPoolCreation/NoPayment.tsx
@@ -36,7 +36,7 @@ const NoPayment: React.FC<IProps> = () => {
         {nftStore.totalPuzzleNftsAmount == null || vm.loading ? (
           <Loading big />
         ) : (
-          `Buy for ${vm.puzzleNFTPrice} TPUZZLE`
+          `Buy for ${vm.puzzleNFTPrice} PUZZLE`
         )}
       </Button>
     </Root>
