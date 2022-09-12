8eebfd2a7ceb6874fb814e1e11bc10adcc99a52a
Vladimir <izhur27@gmail.com>
2022-09-12 18:04:31 +0300

    Update aggregatorService.ts

--- src/services/aggregatorService.ts
+++ src/services/aggregatorService.ts
@@ -29,7 +29,7 @@ const aggregatorService = {
     assetId1: string,
     amount: BN
   ): Promise<ICalcResponse> => {
-    const url = `https://waves.puzzle-aggr-api.com/aggregator/calc?token0=${assetId0}&token1=${assetId1}&amountIn=${amount.toString()}`;
+    const url = `https://164.92.199.3:7000/aggregator/calc?token0=${assetId0}&token1=${assetId1}&amountIn=${amount.toString()}`;
     const { data } = await axios.get(url);
     return data;
   },
