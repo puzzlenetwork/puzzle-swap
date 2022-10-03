f71f634db4ba2b3a1a058b0170520d2748390aa4
Олексiй <alexnagorny.an@gmail.com>
2022-10-03 18:16:39 +0100

    aggr link

--- src/services/aggregatorService.ts
+++ src/services/aggregatorService.ts
@@ -29,8 +29,8 @@ const aggregatorService = {
     assetId1: string,
     amount: BN
   ): Promise<ICalcResponse> => {
-    // const url = `https://waves.puzzle-aggr-api.com/aggregator/calc?token0=${assetId0}&token1=${assetId1}&amountIn=${amount.toString()}`;
-    const url = `https://stage.puzzle-aggr-api.com/aggregator/calc?token0=${assetId0}&token1=${assetId1}&amountIn=${amount.toString()}`;
+    const url = `https://waves.puzzle-aggr-api.com/aggregator/calc?token0=${assetId0}&token1=${assetId1}&amountIn=${amount.toString()}`;
+    // const url = `https://stage.puzzle-aggr-api.com/aggregator/calc?token0=${assetId0}&token1=${assetId1}&amountIn=${amount.toString()}`;
     const { data } = await axios.get(url, {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_AGGREGATOR_KEY}`,
