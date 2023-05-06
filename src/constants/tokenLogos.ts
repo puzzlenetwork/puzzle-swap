f50c436cf9b4c239c6ff3beac91e8073f155a871
Ekaterino <izhur27@gmail.com>
2023-05-06 22:17:03 +0800

    adding NEXT WEEK PEPE TOKEN

--- src/constants/tokenLogos.ts
+++ src/constants/tokenLogos.ts
@@ -77,6 +77,7 @@ import USDTWXG from "@src/assets/tokens/USDTWXG.svg";
 import USDCWXG from "@src/assets/tokens/USDCWXG.svg";
 import WBTCWXG from "@src/assets/tokens/WBTCWXG.svg";
 import BURNXTN from "@src/assets/tokens/BURNXTN.svg";
+import NEXT_WEEK_PEPE from "@src/assets/tokens/NEXTWEEKPEPE.png";
 
 const tokenLogos: Record<string, string> = {
   DUXPLORER: duxplorerLogo,
@@ -159,6 +160,7 @@ const tokenLogos: Record<string, string> = {
   VVUSDNLP: usdnViresVested,
   SWAVES: SWAVES,
   TND: tndLogo,
+  NEXT_WEEK_PEPE: NEXT_WEEK_PEPE,
   SPICE: "https://wavesducks.com/static/img/token/spice.svg",
 };
 
