686efb0930aa46b2db3c9792b7ddcf3e5a5397ba
lzhabo <52199564+lzhabo@users.noreply.github.com>
2025-07-08 18:06:03 +0100

    open ranges

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,12 @@ import Landing from "@screens/Landing";
 import Paper from "@screens/Paper";
 import LoginScreen from "./screens/LoginScreen";
 import { usePageTitle } from "./usePageTitle";
-// import AllRanges from "@screens/Ranges/AllRanges";
-// import RangeDetails from "@src/screens/Ranges/RangeDetails";
-// import DepositToRange from "./screens/Ranges/DepositToRange";
-// import WithdrawFromRange from "./screens/Ranges/WithdrawFromRange";
-// import CreateRange from "./screens/Ranges/CreateRange";
-// import TradeInRange from "./screens/Ranges/TradeInRange";
+import AllRanges from "@screens/Ranges/AllRanges";
+import RangeDetails from "@src/screens/Ranges/RangeDetails";
+import DepositToRange from "./screens/Ranges/DepositToRange";
+import WithdrawFromRange from "./screens/Ranges/WithdrawFromRange";
+import CreateRange from "./screens/Ranges/CreateRange";
+import TradeInRange from "./screens/Ranges/TradeInRange";
 
 const Root = styled(Column)`
   width: 100%;
@@ -107,14 +107,17 @@ const App: React.FC = () => {
         <Route path="*" element={<NotFound />} />
 
         {/*Ranges block*/}
-        {/*<Route path={ROUTES.RANGES} element={<AllRanges />} />*/}
-        {/*<Route path={ROUTES.RANGES_DETAILS} element={<RangeDetails />} />*/}
-        {/*<Route path={ROUTES.RANGES_DEPOSIT} element={<DepositToRange />} />*/}
-        {/*<Route path={ROUTES.RANGES_DEPOSIT_ONE_TOKEN} element={<DepositToRange />} />*/}
-        {/*<Route path={ROUTES.RANGES_WITHDRAW} element={<WithdrawFromRange />} />*/}
-        {/*<Route path={ROUTES.RANGES_CREATE} element={<CreateRange />} />*/}
-        {/*<Route path={ROUTES.RANGES_TRADE} element={<TradeInRange />} />*/}
-        {/*<Route path={ROUTES.USER_RANGES} element={<div>USER_RANGES</div>} />*/}
+        <Route path={ROUTES.RANGES} element={<AllRanges />} />
+        <Route path={ROUTES.RANGES_DETAILS} element={<RangeDetails />} />
+        <Route path={ROUTES.RANGES_DEPOSIT} element={<DepositToRange />} />
+        <Route
+          path={ROUTES.RANGES_DEPOSIT_ONE_TOKEN}
+          element={<DepositToRange />}
+        />
+        <Route path={ROUTES.RANGES_WITHDRAW} element={<WithdrawFromRange />} />
+        <Route path={ROUTES.RANGES_CREATE} element={<CreateRange />} />
+        <Route path={ROUTES.RANGES_TRADE} element={<TradeInRange />} />
+        <Route path={ROUTES.USER_RANGES} element={<div>USER_RANGES</div>} />
       </Routes>
       <WalletModal
         onClose={() => accountStore.setWalletModalOpened(false)}
