20f93d1548363b9eef791e9733613caa46e639a2
Laloww <lalow.main@gmail.com>
2026-01-27 11:20:17 +0300

    feat: fix url

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -73,7 +73,7 @@ export interface IDeveloper {
   role: string;
 }
 
-export const IDEAS_API_URL = process.env.REACT_APP_IDEAS_API_URL || "https://stage2.puzzle.name/ideas-api";
+export const IDEAS_API_URL = process.env.REACT_APP_IDEAS_API_URL || "https://stage1.puzzle.name/ideas-api";
 
 export const PRODUCTS = {
   SWAP: "https://swap.puzzle.network/",
